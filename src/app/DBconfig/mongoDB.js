579c81e4a869f02f79150e8cf06bcfd0bc619d3e
alielrayes <engali7assan@yahoo.com>
2023-12-09 21:34:38 +0200

    lesson7

--- src/app/DBconfig/mongoDB.js
+++ src/app/DBconfig/mongoDB.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
  
 export const connectMongoDB = async () => {
   try {
-    await mongoose.connect("mongodb+srv://devali:mE5DYtcLoDTY7l0O@cluster0.zgdd5xv.mongodb.net/?retryWrites=true&w=majority");
+    await mongoose.connect(process.env.MONGODB_URL);
     console.log("connected to MongoDB");
   } catch (error) {
     console.log("ERROR WITH CONNECTING  MongoDB", error);
