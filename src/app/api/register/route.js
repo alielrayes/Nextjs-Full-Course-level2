100ef400d6f34e43d0ff418a20437ac7748f57a7
alielrayes <engali7assan@yahoo.com>
2023-12-11 17:56:40 +0200

    lesson12

--- src/app/api/register/route.js
+++ src/app/api/register/route.js
@@ -1,43 +1,28 @@
-import UserModal from "app/DBconfig/models/user"
-import { connectMongoDB } from "app/DBconfig/mongoDB"
-import { NextResponse } from "next/server"
-
-
-
-
-
+import UserModal from "app/DBconfig/models/user";
+import { connectMongoDB } from "app/DBconfig/mongoDB";
+import { NextResponse } from "next/server";
+import bcrypt from "bcrypt";
 
 export async function POST(request) {
-// 1- Receive data from Front-end
-const objFromFrontEnd = await request.json()
-console.log("*******************************************")
-console.log(objFromFrontEnd)
+  // 1- Receive data from Front-end
+  const objFromFrontEnd = await request.json();
+  console.log(objFromFrontEnd);
 
-// 2- connect to DB
-await connectMongoDB()
+  // 2- connect to DB
+  await connectMongoDB();
 
-// 3- Try to Store obj to DB
-await UserModal.create({
-  name: objFromFrontEnd.name,
-  email: objFromFrontEnd.email,
-  password: objFromFrontEnd.password,
-})
+  // 3- Hashing password with bcrypt.js
+  console.log("*****************    salt   **************************");
+  const salt = await bcrypt.genSalt();
+  const hashedPassword = await bcrypt.hash(objFromFrontEnd.password, salt);
 
+  // 4- Try to Store obj to DB
+  await UserModal.create({
+    name: objFromFrontEnd.name,
+    email: objFromFrontEnd.email,
+    password: hashedPassword,
+  });
 
-// 4- Go back to frontend
-return NextResponse.json({})
+  // 5- Go back to frontend
+  return NextResponse.json({});
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
